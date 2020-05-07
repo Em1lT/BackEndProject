8c52539a814c7cc904354590f4f41232448d3514
timpec <timpe96@hotmail.com>
2020-05-07 17:29:02 +0300

    bug

--- development.js
+++ development.js
@@ -4,7 +4,5 @@ module.exports = (app,port) => {
     app.use((req, res, next) => {
         next();
     })
-    app.listen(port, () => {  
-        logger.info(`App has started and is running on port:  ${port}!`)
-    })
+    app.listen(port)
 }
